c883f4d7d88c29d2caa544af8746a90d6acdef13
BrixtinW <brixtinlwalker@gmail.com>
2024-03-16 20:05:21 -0600

    login, logout, and register complete

---
+++ client/src/main/java/ui/REPLS/PostloginUI.java
@@ -0,0 +1,48 @@
+package ui.REPLS;
+
+import ui.ServerFacade;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+import static ui.EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY;
+
+public class PostloginUI extends REPL {
+
+    public static String authToken = null;
+
+    @Override
+    protected Boolean evaluate(String[] parsedInput) {
+
+        switch (parsedInput[0]) {
+            case "help":
+                System.out.println( SET_TEXT_COLOR_RED + "\thelp" + SET_TEXT_COLOR_LIGHT_GREY + " - well it seems to be working for you so far. Read and learn." + SET_TEXT_COLOR_RED + "\n\tquit" + SET_TEXT_COLOR_LIGHT_GREY + " - Exit the program." + SET_TEXT_COLOR_RED + "\n\tlogin <USERNAME> <PASSWORD>" + SET_TEXT_COLOR_LIGHT_GREY + " - abandon hope all ye who enter here..." + SET_TEXT_COLOR_RED + "\n\tregister <USERNAME> <PASSWORD> <EMAIL>" + SET_TEXT_COLOR_LIGHT_GREY + " - Joooiiin ussssssss :]");
+                break;
+            case "logout":
+                ServerFacade.logout(authToken);
+                return true;
+            case "create":
+//                    NOT FINISHED
+                break;
+            case "list":
+//                    NOT FINISHED
+                break;
+            case "join":
+//                    NOT FINISHED
+                break;
+            case "observe":
+//                    NOT FINISHED
+                break;
+            default:
+                System.out.println("Ya trash cuz.\ntype help for God's sake");
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+}
---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,125 @@
+package ui;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+public class ServerFacade {
+
+    private static final String SERVER_URL = "http://localhost:8080";
+
+    public static String[] login(String[] parsedInput) {
+        System.out.println(parsedInput[0] + " " + parsedInput[1] + " " +  parsedInput[2]);
+
+        // Create JSON payload for the request
+        String jsonPayload = String.format("{\"username\": \"%s\", \"password\": \"%s\"}", parsedInput[1], parsedInput[2]);
+
+        JsonObject jsonObject = connect("POST", "/session", jsonPayload, null);
+
+        if (jsonObject == null) { return new String[0]; }
+
+        String name = jsonObject.get("username").getAsString();
+        String authToken = jsonObject.get("authToken").getAsString();
+        return new String[] { name, authToken };
+
+    }
+
+
+    public static String[] register(String[] parsedInput){
+        System.out.println(parsedInput[0] + " " + parsedInput[1] + " " +  parsedInput[2] + " " +  parsedInput[3]);
+
+        // Create JSON payload for the request
+        String jsonPayload = String.format("{\"username\": \"%s\", \"password\": \"%s\", \"email\": \"%s\"}", parsedInput[1], parsedInput[2],  parsedInput[3]);
+
+            JsonObject jsonObject = connect("POST", "/user", jsonPayload, null);
+
+            if (jsonObject == null) { return new String[0]; }
+
+            String name = jsonObject.get("username").getAsString();
+            String authToken = jsonObject.get("authToken").getAsString();
+            return new String[] { name, authToken };
+
+    }
+
+    public static void logout(String authToken) {
+
+        System.out.println(authToken);
+
+        JsonObject jsonObject = connect("DELETE", "/session", null, authToken);
+
+        System.out.println(jsonObject.toString())
+
+        return;
+
+        }
+
+
+    private static JsonObject connect(String endpointType, String api, String jsonPayload, String authToken) {
+
+        HttpURLConnection connection = null;
+
+        try {
+
+            URL url = new URL(SERVER_URL + api);
+
+            // Open a connection to the server
+            connection = (HttpURLConnection) url.openConnection();
+
+            // Set request method to POST
+            connection.setRequestMethod(endpointType);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+//            if (authToken != null) {
+                connection.setRequestProperty("Authorization", authToken);
+//            }
+
+
+            // Write JSON payload to the connection's output stream
+            try (OutputStream outputStream = connection.getOutputStream()) {
+                if (jsonPayload != null) {
+                    byte[] input = jsonPayload.getBytes(StandardCharsets.UTF_8);
+                    outputStream.write(input, 0, input.length);
+                }
+            }
+
+            // Get the response code from the server
+            int responseCode = connection.getResponseCode();
+
+            // Read the response from the server
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                return JsonParser.parseString(response.toString()).getAsJsonObject();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // Close the connection in the finally block to ensure it's always closed
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        return null;
+    }
+//
+//    }
+//
+//    public static void createGame(String[] parsedInput){
+//
+//    }
+//
+//    public static String listGames(String[] parsedInput) throws IOException {
+
+}
+
