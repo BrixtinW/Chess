e06815fac53c948db4b1d5b918519c13ef758ac2
BrixtinW <brixtinlwalker@gmail.com>
2024-03-18 12:05:16 -0600

    prelogin and postlogin implemented. only Gameplay UI left

--- client/src/main/java/ui/REPLS/GameplayUI.java
+++ client/src/main/java/ui/REPLS/GameplayUI.java
@@ -1,4 +1,30 @@
 package ui.REPLS;
 
-public class GameplayUI {
+import ui.ServerFacade;
+
+import static ui.EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY;
+import static ui.EscapeSequences.SET_TEXT_COLOR_RED;
+
+public class GameplayUI extends REPL{
+
+    @Override
+    protected Boolean evaluate(String[] parsedInput) {
+
+        switch (parsedInput[0]) {
+            case "help":
+                break;
+            case "logout":
+                return true;
+            case "create":
+                break;
+            case "list":
+                break;
+            case "join":
+                break;
+            default:
+                System.out.println("Ya trash cuz.\ntype help for God's sake");
+        }
+        return false;
+    }
+
 }
